276f48f672b73c657436eec898e8f0fa55739cac
Daniel Endres <danielendres@gmail.com>
2017-06-26 15:43:12 -0700

    testing darksky api call to retrieve phoenix weather, add error handling for darksky return

--- app.js
+++ app.js
@@ -1,24 +1,42 @@
-const yargs = require('yargs');
+// const yargs = require('yargs');
+//
+// const geocode = require('./geocode/geocode.js');
+//
+// const argv = yargs
+//   .options({
+//     a: {
+//       demand: true,
+//       alias: 'address',
+//       describe: 'Address to fetch weather for',
+//       string: true
+//     }
+//   })
+//   .help()
+//   .alias('help', 'h')
+//   .argv;
+//
+// geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+//   if (errorMessage) {
+//     console.log(errorMessage);
+//   } else {
+//     console.log(JSON.stringify(results, undefined, 2));
+//   }
+// });
 
-const geocode = require('./geocode/geocode.js');
 
-const argv = yargs
-  .options({
-    a: {
-      demand: true,
-      alias: 'address',
-      describe: 'Address to fetch weather for',
-      string: true
-    }
-  })
-  .help()
-  .alias('help', 'h')
-  .argv;
+// 84a0a22682ae951e91aed604901cc94e
+// https://api.darksky.net/forecast/84a0a22682ae951e91aed604901cc94e/37.8267,-122.4233
 
-geocode.geocodeAddress(argv.address, (errorMessage, results) => {
-  if (errorMessage) {
-    console.log(errorMessage);
+
+const request = require('request');
+
+request({
+  url: `https://api.darksky.net/forecast/84a0a22682ae951e91aed604901cc94e/33.6364503,-112.0118669`,
+  json: true
+}, (error, response, body) => {
+  if (!error && response.statusCode === 200) {
+    console.log(body.currently.temperature);
   } else {
-    console.log(JSON.stringify(results, undefined, 2));
+    console.log('Unable to fetch weather');
   }
 });
