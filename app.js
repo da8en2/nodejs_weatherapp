54e4d7e86451b3b2de00857bd10ab817a72f8c7b
Daniel Endres <danielendres@gmail.com>
2017-06-26 16:07:13 -0700

    finish call to weather api to include lat,lng return values from geocode api

--- app.js
+++ app.js
@@ -20,14 +20,13 @@ geocode.geocodeAddress(argv.address, (errorMessage, results) => {
   if (errorMessage) {
     console.log(errorMessage);
   } else {
-    console.log(JSON.stringify(results, undefined, 2));
-  }
-});
-
-weather.getWeather(33.6364503,-112.0118669, (errorMessage, weatherResults) => {
-  if (errorMessage) {
-    console.log(errorMessage);
-  } else {
-    console.log(JSON.stringify(weatherResults, undefined, 2));
+    console.log(results.address);
+    weather.getWeather(results.latitude,results.longitude, (errorMessage, weatherResults) => {
+      if (errorMessage) {
+        console.log(errorMessage);
+      } else {
+        console.log(`It's currently ${weatherResults.temperature}. It feels like ${weatherResults.apparentTemperature}.`);
+      }
+    });
   }
 });
